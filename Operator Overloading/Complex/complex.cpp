fe1dd20859366a4c3cbce4e3010e34c21c10b121
Jayesh <en001507@gmail.com>
2024-09-04 15:05:08 +0530

    Assignment 01

---
+++ Operator Overloading/Complex/complex.cpp
@@ -0,0 +1,74 @@
+#include "complex.h"
+#include <iostream>
+using namespace std;
+
+Complex::Complex() : real(1), img(1) {};
+
+Complex::Complex(int real, int img) : real(real), img(img) {};
+
+void Complex::display()
+{
+    if (img < 0)
+    {
+        cout << endl
+             << real << img << "i";
+    }
+    else
+    {
+        cout << endl
+             << real << "+" << img << "i";
+    }
+}
+void Complex::accept()
+{
+    cout << endl
+         << "Enter The real and imaginary number in space separated form:\n"
+         << endl;
+    cin >> this->real >> this->img;
+}
+
+Complex Complex::operator+(Complex &obj)
+{
+    Complex temp;
+    temp.real = this->real + obj.real;
+    temp.img = this->img + obj.img;
+
+    return temp;
+}
+
+Complex Complex::operator-(Complex &obj)
+{
+    Complex temp;
+    temp.real = this->real - obj.real;
+    temp.img = this->img - obj.img;
+
+    return temp;
+}
+
+Complex Complex::operator*(Complex &obj)
+{
+    Complex temp;
+
+    temp.real = this->real * obj.real;
+    temp.img = this->img * obj.img;
+    return temp;
+}
+
+// int a is just given for distinguish it from pre-increment no specific reason, just acts as place holder
+Complex Complex::operator++(int a)
+{
+    // acts as increment operator
+    Complex temp;
+    temp.real = this->real++;
+    temp.img = this->img++;
+
+    return temp;
+}
+Complex Complex::operator++()
+{
+    Complex temp;
+    temp.real = ++this->real;
+    temp.img = ++this->img;
+
+    return temp;
+}
