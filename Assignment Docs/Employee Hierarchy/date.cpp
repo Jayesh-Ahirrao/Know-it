499d4ddb284e8dd9a9161a4b5b2ae24a947c516a
Jayesh <en001507@gmail.com>
2024-09-09 22:24:32 +0530

    employee inheritance

---
+++ Assignment Docs/Employee Hierarchy/date.cpp
@@ -0,0 +1,29 @@
+#include "date.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+Date::Date()
+{
+    this->day = this->month = 1;
+    this->year = 2014;
+}
+
+Date::Date(const int day, const int month, const int year)
+{
+    this->day = day;
+    this->month = month;
+    this->year = year;
+}
+
+Date::Date(const int dateAndMonth)
+{
+    this->day = this->month = dateAndMonth;
+    this->year = 2014;
+}
+
+void Date::display(const char *format = "Date: ")
+{
+    cout << endl
+         << format << this->day << "/" << this->month << "/" << this->year;
+}
